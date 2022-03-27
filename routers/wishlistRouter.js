ac7c994df4fd18353d9470c0fd0749f60ef4bff4
Shiv Desai <SHIVDESAI612@gmail.com>
2022-03-27 12:19:03 -0300

    merge with main

---
+++ routers/wishlistRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const wishlistRouter = express.Router();
+const wishlistController = require("../controllers/wishlistController");
+
+wishlistRouter.get("/:id", wishlistController.getWishlistListByUserIdorId);
+wishlistRouter.post("/", wishlistController.addWishlist);
+wishlistRouter.put("/:id", wishlistController.updateWishlist);
+wishlistRouter.delete("/:id", wishlistController.deleteWishlist);
+
+module.exports = wishlistRouter;
