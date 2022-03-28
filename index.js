e465d1a682fdf59903380dbbe66c4d38a32ba5aa
Shiv Desai <SHIVDESAI612@gmail.com>
2022-03-28 08:11:41 -0300

    status code change

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const allowedOrigins = [
   "http://localhost:3003",
   "http://localhost:3001",
   "http://localhost:3002",
+  "http://localhost:3000",
 ];
 const corsOptions = {
   origin: allowedOrigins,
